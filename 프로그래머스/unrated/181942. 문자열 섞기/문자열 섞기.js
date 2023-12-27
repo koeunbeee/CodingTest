0a7202093ba96205b23e0e987ce3f7ef68ea63db
koeunbeee <110508913+koeunbeee@users.noreply.github.com>
2023-12-27 10:31:00 +0900

    [level unrated] Title: 문자열 섞기, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.js
@@ -0,0 +1,9 @@
+function solution(str1, str2) {
+    var answer = '';
+    
+    for(i=0; i<str1.length; i++){
+        answer += str1.substring(i,i+1) + str2.substring(i,i+1);
+    }
+        
+    return answer;
+}
