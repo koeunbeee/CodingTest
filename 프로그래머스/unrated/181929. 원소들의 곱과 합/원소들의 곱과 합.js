fbbf13f83be27ec83428c10d7392a70b389069da
koeunbeee <110508913+koeunbeee@users.noreply.github.com>
2024-01-11 01:05:29 +0900

    [level unrated] Title: 원소들의 곱과 합, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -0,0 +1,18 @@
+function solution(num_list) {
+    var answer = 0;
+    let multiplication = 1;
+    let squared = 0;
+    
+    for(let i=0; i<num_list.length; i++) {
+        multiplication *= num_list[i];
+    }
+    
+    for(let i=0; i<num_list.length; i++) {
+        squared += num_list[i];
+    }
+    
+    multiplication < squared*squared ? 
+        answer=1 : answer=0;
+
+    return answer;
+}
