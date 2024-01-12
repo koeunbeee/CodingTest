d3eb4b766acafe6c818bb64ba40858d5e25e31f3
koeunbeee <110508913+koeunbeee@users.noreply.github.com>
2024-01-12 16:37:28 +0900

    [level Lv.0] Title: 수 조작하기 1, Time: 2.74 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181926. 수 조작하기 1/수 조작하기 1.js
@@ -0,0 +1,23 @@
+function solution(n, control) {
+    var answer = 0;
+    let arr = control.split("");
+    
+    for(let i=0; i < arr.length; i++){
+        switch (arr[i]){
+            case "w":
+                n += 1
+                break;
+            case "s":
+                n -= 1
+                break;
+            case "d":
+                n += 10
+                break;
+            case "a":
+                n -= 10
+                break;
+        }
+    }    
+    answer = n;
+    return answer;
+}
