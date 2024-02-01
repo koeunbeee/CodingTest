0f8c651b39091af1fa5a94ef0ceaf74138c6fb38
koeunbeee <110508913+koeunbeee@users.noreply.github.com>
2024-02-01 17:48:10 +0900

    [level 0] Title: 배열 만들기 4, Time: 6.50 ms, Memory: 37.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -0,0 +1,15 @@
+function solution(arr) {
+    var stk = [];
+    for(let i=0; i<arr.length; i++){
+        if(!stk.length) {
+            stk.push(arr[i]);
+            console.log(stk)
+        } else if (stk[stk.length-1] < arr[i]){
+            stk.push(arr[i]);            
+        } else if(stk[stk.length-1] >= arr[i]){
+            stk.pop();
+            i--
+        }
+    }
+    return stk;
+}
